7e344aa91758883aa4a6021960dc2eab852fbc53
MaximilianUE <maximilian.becher@ueberbit.de>
2018-07-22 21:34:21 +0200

    add global template

--- web/src/js/main.js
+++ web/src/js/main.js
@@ -1,4 +1,18 @@
 
+import Handlebars from 'handlebars/lib/handlebars.js';
+
 /* Import parts*/
 import './../parts/header/header';
 import './../parts/footer/footer';
+
+
+/*create body*/
+
+//compile main template
+let compiledTemplate = Handlebars.compile(document.getElementById("site").innerHTML);
+
+//fill main template
+let div = document.createElement('div');
+div.classList.add('site');
+div.innerHTML = compiledTemplate();
+document.body.appendChild(div);
