79b5e5a0d55b66f16417891c6b3de41c61e16ae6
Maximilian Becher <maximilian.becher@ueberbit.de>
2018-08-21 17:55:18 +0200

    add stylelint config

--- web/postcss.config.js
+++ web/postcss.config.js
@@ -3,6 +3,8 @@ const autoprefixer = require("autoprefixer");
 module.exports = {
   syntax: 'postcss-scss',
   plugins: [
+      require('stylelint'),
+      require('postcss-reporter')({ clearReportedMessages: true }),
       require('precss'),
       require('postcss-preset-env'),
       autoprefixer({ grid: true }),
