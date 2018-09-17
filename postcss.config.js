f7278f588b6f71dd03f869ab0c38c9c432e6adb8
Maximilian Becher <maximilian.becher@ueberbit.de>
2018-09-17 14:54:22 +0200

    postcss: add small descr. for package reason

--- postcss.config.js
+++ postcss.config.js
@@ -4,12 +4,12 @@ const postcssNormalize = require('postcss-normalize');
 module.exports = {
   syntax: 'postcss-scss',
   plugins: [
-      require('stylelint'),
-      require('postcss-reporter')({ clearReportedMessages: true }),
-      require('precss'),
+      require('stylelint'), //console based linting
+      require('postcss-reporter')({ clearReportedMessages: true }), // ?
       postcssNormalize({ forceImport: true }), //only use normalize for supported browsers
+      require('precss'), //allows sass like structure
       require('postcss-preset-env'),
       autoprefixer({ grid: true }),
-      require('cssnano'),
+      require('cssnano'), //minifies css
   ]
 }
