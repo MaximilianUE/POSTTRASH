2f3b3c3d4ccae0750d1d3e9816fd4b98d1686374
Maximilian Becher <maximilian.becher@ueberbit.de>
2019-04-24 12:26:29 +0200

    📦 add babel polyfills and upgrade precss - babel seperated their polyfills into their cores - includes polyfill cores - fix conflicting task order in postcss.config

--- postcss.config.js
+++ postcss.config.js
@@ -1,16 +1,17 @@
 const postcssNormalize = require("postcss-normalize");
 const postcssPresetEnv = require("postcss-preset-env");
 
+//executed for bottom to top!
 module.exports = {
   syntax: "postcss-scss",
   plugins: [
     require("stylelint"), //console based linting
     require("postcss-reporter")({ clearReportedMessages: true }), // ?
-    postcssNormalize({ forceImport: true }), //only use normalize for supported browsers
-    require("precss"), //allows sass like structure
     postcssPresetEnv({
       //handles support for old browsers
       autoprefixer: { grid: "autoplace" }
-    })
+    }),
+    postcssNormalize({ forceImport: true }), //only use normalize for supported browsers
+    require("precss") //allows sass like structure
   ]
 };
