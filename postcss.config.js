a8fb1fa74d81e8914928c340f3192364a9ba70d8
Maximilian Becher <maximilian.becher@ueberbit.de>
2019-04-25 12:34:10 +0200

    (re)add cssnano to postcss pipeline

--- postcss.config.js
+++ postcss.config.js
@@ -12,6 +12,7 @@ module.exports = {
       autoprefixer: { grid: "autoplace" }
     }),
     postcssNormalize({ forceImport: true }), //only use normalize for supported browsers
-    require("precss") //allows sass like structure
+    require("precss"), //allows sass like structure
+    require("cssnano") //minifies css
   ]
 };
