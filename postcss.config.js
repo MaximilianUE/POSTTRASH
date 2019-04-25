f07f869e77a96ad1a7bc234b7a847ce9eaeafa64
Maximilian Becher <maximilian.becher@ueberbit.de>
2019-04-25 12:47:08 +0200

    move order of postcss-reporter plugin - make sure it will combine the log outputs for all the logs following it

--- postcss.config.js
+++ postcss.config.js
@@ -1,18 +1,19 @@
 const postcssNormalize = require("postcss-normalize");
 const postcssPresetEnv = require("postcss-preset-env");
+const postcssReporter = require("postcss-reporter");
 
 //executed for bottom to top!
 module.exports = {
   syntax: "postcss-scss",
   plugins: [
     require("stylelint"), //console based linting
-    require("postcss-reporter")({ clearReportedMessages: true }), // ?
     postcssPresetEnv({
       //handles support for old browsers
       autoprefixer: { grid: "autoplace" }
     }),
     postcssNormalize({ forceImport: true }), //only use normalize for supported browsers
     require("precss"), //allows sass like structure
-    require("cssnano") //minifies css
+    require("cssnano"), //minifies css
+    postcssReporter({ clearReportedMessages: true }) // combines all plugin logs
   ]
 };
