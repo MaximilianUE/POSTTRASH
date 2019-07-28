3c73947f88ec0a4405c1ff5b57f3f85f79faca68
Maximilian Becher <maximilian.becher@ueberbit.de>
2019-07-28 20:59:38 +0200

    Rename config values, simplify getting default config.
    Cleanup config file, add more consistent naming syntax.

--- webpack.config.js
+++ webpack.config.js
@@ -12,90 +12,39 @@ const path = require("path");
 
 /* -------------- Settings -------------- */
 
-const spConfigFile = cosmiconfig("simpleplate").searchSync();
+const simplePlateConfigFile = cosmiconfig("simpleplate").searchSync();
 
-let spConfig;
-if (!spConfigFile) {
-  spConfig = {};
-} else {
-  spConfig = spConfigFile.config;
+let simplePlateConfig = {};
+if (simplePlateConfigFile) {
+  simplePlateConfig = simplePlateConfigFile.config;
 }
 
-const { paths } = spConfig;
-const { input, output } = paths || {};
+// use object destructuring to set a custom value or fallback on the defaults.
+
+const { paths: spc_paths } = simplePlateConfig;
+const { input: spc_input, output: spc_output } = spc_paths || {};
 const {
-  htmlEntry = "./*.html",
-  cssEntry = "./src/css/main.scss",
-  mainJs = "./src/js/main.js",
-  polyfillJs = "./src/js/polyfills.js",
-  imgEntry = "./src/img",
-  iconsEntry = "./src/img/icons/",
-  fontsEntry = "./src/fonts"
-} = input || {};
+  html: htmlFolder = "./*.html",
+  css: cssEntry = "./src/css/main.scss",
+  js: jsEntry = "./src/js/main.js",
+  polyfill: polyfillEntry = "./src/js/polyfills.js",
+  images: imageFolder = "./src/img",
+  icons: iconFolder = "./src/img/icons/",
+  fonts: fontFolder = "./src/fonts"
+} = spc_input || {};
 
-const { outputFolder = "./dist/", imageOutput = "./img/" } = output || {};
-
-console.log("htmlEntry: " + htmlEntry);
-console.log("outputFolder: " + outputFolder);
-
-/*
-function getSimplePlateConfig() {
-  const configFile = explorer.searchSync();
-  const config = configFile.config;
-  return config;
-}
-
-const simplePlateConfig = getSimplePlateConfig();
-
-const spcDefaultOpts = {
-  paths: {
-    input: {
-      htmlEntry: "./*.html",
-      cssEntry: "./src/css/main.scss",
-      mainJs: "./src/js/main.js",
-      polyfillJs: "./src/js/polyfills.js",
-      imgEntry: "./src/img",
-      iconsEntry: "./src/img/icons/",
-      fontsEntry: "./src/fonts"
-    },
-    output: {
-      outputFolder: "./dist/",
-      imageOutput: "./img/"
-    }
-  }
-};
-
-const spcDefaultPaths = spcDefaultOpts.paths;
-const spcDefaultPathInput = spcDefaultPaths.input;
-const spcDefaultPathOutput = spcDefaultPaths.output;
-
-const spcPaths = simplePlateConfig.paths;
-const spcPathInput = spcPaths.input;
-const spcPathOutput = spcPaths.output;
-
-const htmlEntry = spcPathInput.htmlEntry || spcDefaultPathInput.htmlEntry;
-const cssEntry = spcPathInput.cssEntry || spcDefaultPathInput.cssEntry;
-const mainJs = spcPathInput.mainJs || spcDefaultPathInput.mainJs;
-const polyfillJs = spcPathInput.polyfillJs || spcDefaultPathInput.polyfillJs;
-const imgEntry = spcPathInput.imgEntry || spcDefaultPathInput.imgEntry;
-const iconsEntry = spcPathInput.iconsEntry || spcDefaultPathInput.iconsEntry;
-const fontsEntry = spcPathInput.fontsEntry || spcDefaultPathInput.fontsEntry;
-
-console.log(spcPathOutput.imageOutput);
-
-const outputFolder =
-  spcPathOutput.outputFolder || spcDefaultPathOutput.outputFolder;
-const imageOutput =
-  spcPathOutput.imageOutput || spcDefaultPathOutput.imageOutput;
-*/
+const {
+  build: outputFolder = "./dist/",
+  images: imageOutputFolder = "./img/"
+} = spc_output || {};
 
 /* -------------------------------------- */
 
 module.exports = (env, argv) => ({
   entry: {
     main: cssEntry,
-    polyfills: polyfillJs,
-    index: mainJs
+    polyfills: polyfillEntry,
+    index: jsEntry
   },
   output: {
     path: path.join(__dirname, outputFolder),
@@ -133,16 +82,16 @@ module.exports = (env, argv) => ({
       filename: "[name].css"
     }),
     new CopyWebpackPlugin([
-      { from: imgEntry, to: "img" },
-      { from: htmlEntry },
-      { from: fontsEntry, to: "fonts" }
+      { from: imageFolder, to: "img" },
+      { from: htmlFolder },
+      { from: fontFolder, to: "fonts" }
     ]),
-    new SVGSpritemapPlugin([iconsEntry + "*.svg"], {
+    new SVGSpritemapPlugin([iconFolder + "*.svg"], {
       sprite: {
         prefix: "icon-"
       },
       output: {
-        filename: imageOutput + "spritesheet.svg",
+        filename: imageOutputFolder + "spritesheet.svg",
         svg4everybody: true
       }
     }),
